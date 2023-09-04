af6d54f2e738a66eb1d2681b73e6ea71c13b793f
Kynoskefalaj <91989136+Kynoskefalaj@users.noreply.github.com>
2023-09-04 23:59:43 +0200

    DemoPanel + Nodes

---
+++ src/main/java/Node.java
@@ -0,0 +1,35 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Node extends JButton implements ActionListener {
+
+    Node parent;
+    int col;
+    int row;
+    int gCost;
+    int hCost;
+    int fCost;
+    boolean start;
+    boolean goal;
+    boolean solid;
+    boolean open;
+    boolean checked;
+
+    public Node (int col, int row) {
+
+        this.col = col;
+        this.row = row;
+
+        setBackground(Color.black);
+        setForeground(Color.white);
+        addActionListener(this);
+    }
+
+    @Override
+    public void actionPerformed (ActionEvent e) {
+
+        setBackground(Color.orange);
+    }
+}
